ac9be38ef52474693c16a337b4bea1cd8f4cb2a1
MaksAMillion <maxx852@yahoo.com>
2022-05-11 17:58:19 -0700

    Added starting template to fill in regular expressions

--- RegularExpressions/RegExpGolf.js
+++ RegularExpressions/RegExpGolf.js
@@ -1,5 +1,10 @@
 /*
-    For each of the following items, write a regular expression to test whether any of the given substrings occur in a string. The regular expression should match only strings containing one of the substrings described. Do not worry about word boundaries unless explicitly mentioned. When your expression works, see whether you can make it any smaller.
+    For each of the following items, write a regular expression to test 
+    whether any of the given substrings occur in a string. The regular
+    expression should match only strings containing one of the substrings 
+    described. Do not worry about word boundaries unless explicitly 
+    mentioned. When your expression works, see whether you can make it 
+    any smaller.
 
     car and cat
 
@@ -16,3 +21,45 @@
     A word without the letter e (or E)
 
 */
+
+// Fill in the regular expressions
+
+verify(/.../,
+       ["my car", "bad cats"],
+       ["camper", "high art"]);
+
+verify(/.../,
+       ["pop culture", "mad props"],
+       ["plop", "prrrop"]);
+
+verify(/.../,
+       ["ferret", "ferry", "ferrari"],
+       ["ferrum", "transfer A"]);
+
+verify(/.../,
+       ["how delicious", "spacious room"],
+       ["ruinous", "consciousness"]);
+
+verify(/.../,
+       ["bad punctuation ."],
+       ["escape the period"]);
+
+verify(/.../,
+       ["Siebentausenddreihundertzweiundzwanzig"],
+       ["no", "three small words"]);
+
+verify(/.../,
+       ["red platypus", "wobbling nest"],
+       ["earth bed", "learning ape", "BEET"]);
+
+
+function verify(regexp, yes, no) {
+  // Ignore unfinished exercises
+  if (regexp.source == "...") return;
+  for (let str of yes) if (!regexp.test(str)) {
+    console.log(`Failure to match '${str}'`);
+  }
+  for (let str of no) if (regexp.test(str)) {
+    console.log(`Unexpected match for '${str}'`);
+  }
+}
